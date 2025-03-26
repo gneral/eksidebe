000fdf63120ee9877100b7794dbb3c2f174eed31
Muhsin ŞSEVER <gneral@gmail.com>
2025-03-26 17:36:29 +0300

    bug fixed

--- kurulum.bat
+++ kurulum.bat
@@ -15,22 +15,35 @@ echo.
 
 REM Chrome tarayıcısı kontrolü
 echo Chrome tarayıcısı kontrol ediliyor...
+set "CHROME_FOUND=0"
+
+REM Bilinen lokasyonlarda Chrome'u ara
 if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" (
-    echo Chrome tarayıcısı bulundu.
-    set CHROME_PATH="%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
-) else (
-    if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
-        echo Chrome tarayıcısı bulundu.
-        set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
+    echo Chrome tarayıcısı bulundu: %LOCALAPPDATA%\Google\Chrome\Application\chrome.exe
+    set "CHROME_PATH=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+    set "CHROME_FOUND=1"
+) else if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    echo Chrome tarayıcısı bulundu: C:\Program Files\Google\Chrome\Application\chrome.exe
+    set "CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe"
+    set "CHROME_FOUND=1"
+) else if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
+    echo Chrome tarayıcısı bulundu: C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+    set "CHROME_PATH=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    set "CHROME_FOUND=1"
+)
+
+REM Chrome bulunamazsa, doğrudan PATH üzerinden çalıştırmayı dene
+if "%CHROME_FOUND%"=="0" (
+    echo Chrome tarayıcısı standart konumlarda bulunamadı.
+    echo PATH üzerinden Chrome'u çalıştırmayı deneyeceğim.
+    where chrome >nul 2>nul
+    if %ERRORLEVEL% equ 0 (
+        echo Chrome PATH üzerinde bulundu.
+        set "CHROME_CMD=chrome"
+        set "CHROME_FOUND=1"
     ) else (
-        if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
-            echo Chrome tarayıcısı bulundu.
-            set CHROME_PATH="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-        ) else (
-            echo Chrome tarayıcısı bulunamadı.
-            echo Manuel kurulum yapmanız gerekecek.
-            goto MANUEL_KURULUM
-        )
+        echo Chrome bulunamadı. Manuel kurulum yapmanız gerekecek.
+        goto MANUEL_KURULUM
     )
 )
 
@@ -66,7 +79,17 @@ set /p KURULUM_SECIM=Seçiminiz (1-3):
 
 if "%KURULUM_SECIM%"=="1" (
     echo Chrome'da uzantılar sayfası açılıyor...
-    start %CHROME_PATH% "chrome://extensions/"
+    if "%CHROME_FOUND%"=="1" (
+        if defined CHROME_PATH (
+            start "" "%CHROME_PATH%" "chrome://extensions/"
+        ) else (
+            start "" %CHROME_CMD% "chrome://extensions/"
+        )
+    ) else (
+        echo Chrome açılamadı. Lütfen manuel kurulum yapın.
+        goto MANUEL_KURULUM
+    )
+    
     echo.
     echo Lütfen şu adımları takip edin:
     echo 1. Açılan sayfada "Geliştirici modu"nu etkinleştirin (sağ üst köşede)
@@ -76,7 +99,16 @@ if "%KURULUM_SECIM%"=="1" (
     echo.
     echo EkşiSözlük Debe sayfasını açmak için Enter tuşuna basın.
     pause >nul
-    start %CHROME_PATH% "https://eksisozluk.com/debe"
+    
+    if "%CHROME_FOUND%"=="1" (
+        if defined CHROME_PATH (
+            start "" "%CHROME_PATH%" "https://eksisozluk.com/debe"
+        ) else (
+            start "" %CHROME_CMD% "https://eksisozluk.com/debe"
+        )
+    ) else (
+        echo Chrome açılamadı. Lütfen manuel olarak EkşiSözlük Debe sayfasını açın.
+    )
     goto SON
 ) else if "%KURULUM_SECIM%"=="2" (
     goto MANUEL_KURULUM
